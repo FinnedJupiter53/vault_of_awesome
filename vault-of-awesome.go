3599a545b75fcb2e28e2b42c28e9fa1713b9ab2c
James White <james.white@dfsco.com>
2017-10-15 16:07:38 -0700

    Adding dice rolling support

--- vault-of-awesome.go
+++ vault-of-awesome.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
+func die(max int) int {
+	return rand.Int()%max + 1
+}
+
 func main() {
-	fmt.Println("Welcome to the vault of AWESOME")
+	seed := time.Now().UnixNano()
+	rand.Seed(seed)
+
+	num := flag.Int("num", 1, "number of dice")
+	max := flag.Int("max", 20, "max value of dice")
+
+	if *num <= 0 {
+		fmt.Println("'num' flag must have a number greater than zero")
+	}
+	if *max <= 0 {
+		fmt.Println("'max' flag must have a number greater than zero")
+	}
+
+	for i := 0; i < *num; i++ {
+		fmt.Printf("%v\n", die(*max))
+	}
 }
